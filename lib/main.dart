776fc3f37fc53267817f20becc2bd198e2f46bcc
Bagus Widiyanto <bgswdyt@gmail.com>
2022-04-09 15:10:40 +0700

    added live chat header

--- lib/main.dart
+++ lib/main.dart
@@ -38,67 +38,149 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            children: [
-              const MediaPlayer(),
-              const SizedBox(height: 3),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(13),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.wifi,
-                          color: Colors.red,
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          'LIVE',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 80,
-                        height: 80,
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: 3,
-                          itemBuilder: ((context, index) => const Align(
-                                widthFactor: 0.6,
-                                child: CircleAvatar(
-                                  backgroundColor: Colors.white,
-                                  child: CircleAvatar(
-                                    radius: 18,
-                                    backgroundImage: NetworkImage(
-                                        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'),
-                                  ),
-                                ),
-                              )),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      const Text(
-                        '+420 watching',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  )
-                ],
-              ),
+            children: const [
+              MediaPlayer(),
+              SizedBox(height: 15),
+              LiveStatus(),
+              SizedBox(height: 15),
+              LiveChatStatus(),
             ],
           ),
         ));
+  }
+}
+
+class LiveChatStatus extends StatelessWidget {
+  const LiveChatStatus({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 1.8,
+          color: Colors.grey[200],
+        ),
+        const SizedBox(height: 13),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const ViewerThumbnail(
+              height: 30,
+              width: 60,
+            ),
+            const Text(
+              'OPEN LIVE CHAT',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                border: const Border(
+                  top: BorderSide(color: Colors.grey),
+                  bottom: BorderSide(color: Colors.grey),
+                  left: BorderSide(color: Colors.grey),
+                  right: BorderSide(color: Colors.grey),
+                ),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: const Text(
+                '402',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 13),
+        Container(
+          height: 1.8,
+          color: Colors.grey[200],
+        ),
+      ],
+    );
+  }
+}
+
+class LiveStatus extends StatelessWidget {
+  const LiveStatus({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: 100,
+          height: 50,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(13),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(
+                Icons.wifi,
+                color: Colors.red,
+              ),
+              SizedBox(width: 10),
+              Text(
+                'LIVE',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+        Row(
+          children: const [
+            ViewerThumbnail(),
+            SizedBox(width: 10),
+            Text(
+              '+420 watching',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class ViewerThumbnail extends StatelessWidget {
+  final double? width;
+  final double? height;
+
+  const ViewerThumbnail({
+    Key? key,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width ?? 85,
+      height: height ?? 35,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 3,
+        itemBuilder: ((context, index) => Align(
+              widthFactor: height == null ? 0.7 : 0.5,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: height == null ? 15 : 13,
+                  backgroundImage: const NetworkImage(
+                      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'),
+                ),
+              ),
+            )),
+      ),
+    );
   }
 }
 
