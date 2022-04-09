8768ea8b223d50bd54e84946c1b4da514440c284
Bagus Widiyanto <bgswdyt@gmail.com>
2022-04-09 14:17:49 +0700

    live indicator and watch count added, move media player as a seperated widget, etc

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Watch Party',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Gio\'s Watch Party'),
     );
   }
 }
@@ -29,14 +29,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,57 +39,124 @@ class _MyHomePageState extends State<MyHomePage> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: [
-                    Image.network(
-                        'https://akcdn.detik.net.id/visual/2018/01/08/c35f94d7-385c-4628-a3b7-845ec359a2cf_169.jpeg?w=650'),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          const Icon(
-                            Icons.pause_rounded,
-                            color: Colors.white,
-                          ),
-                          Stack(
-                            children: [
-                              Container(
-                                width: MediaQuery.of(context).size.width * 0.4,
-                                height: 2,
-                                color: Colors.grey[400],
-                              ),
-                              Container(
-                                width: MediaQuery.of(context).size.width * 0.25,
-                                height: 2,
-                                color: Colors.white,
-                              ),
-                            ],
-                          ),
-                          const Text(
-                            '01:05:52',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          const Icon(
-                            Icons.settings,
-                            color: Colors.white,
-                            size: 20,
-                          ),
-                          const Icon(
-                            Icons.fullscreen_rounded,
-                            color: Colors.white,
-                          ),
-                        ],
+              const MediaPlayer(),
+              const SizedBox(height: 3),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(13),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.wifi,
+                          color: Colors.red,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'LIVE',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 80,
+                        height: 80,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 3,
+                          itemBuilder: ((context, index) => const Align(
+                                widthFactor: 0.6,
+                                child: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: CircleAvatar(
+                                    radius: 18,
+                                    backgroundImage: NetworkImage(
+                                        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'),
+                                  ),
+                                ),
+                              )),
+                        ),
                       ),
-                    )
-                  ],
-                ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        '+420 watching',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  )
+                ],
               ),
             ],
           ),
         ));
+  }
+}
+
+class MediaPlayer extends StatelessWidget {
+  const MediaPlayer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Image.network(
+              'https://akcdn.detik.net.id/visual/2018/01/08/c35f94d7-385c-4628-a3b7-845ec359a2cf_169.jpeg?w=650'),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Icon(
+                  Icons.pause_rounded,
+                  color: Colors.white,
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: 2,
+                      color: Colors.grey[400],
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      height: 2,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                const Text(
+                  '01:05:52',
+                  style: TextStyle(color: Colors.white),
+                ),
+                const Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                  size: 20,
+                ),
+                const Icon(
+                  Icons.fullscreen_rounded,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
